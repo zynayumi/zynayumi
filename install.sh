b0680479ddbbff82ab07616458c60fbc8a4f0da7
Nil Geisweiller <ngeiswei@gmail.com>
2020-11-23 07:59:19 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 #
-# Script to build and install Zynayumi on a GNU/Linux system.
+# Script to install zynayumi on a GNU/Linux system.
 
 set -x
 
-# Build
-cd libzynayumi
-mkdir build
-cd build
-cmake ..
-make -j
-cd ../..
-
 # Install
 cd bin
-sudo cp zynayumi /usr/local/bin
-sudo mkdir /usr/local/lib/lv2
-sudo cp -r zynayumi.lv2 /usr/local/lib/lv2
-sudo mkdir /usr/local/lib/dssi
-sudo cp zynayumi-dssi.so /usr/local/lib/dssi
-sudo mkdir /usr/local/lib/vst
-sudo cp zynayumi-vst.so /usr/local/lib/vst
+cp zynayumi /usr/local/bin
+mkdir /usr/local/lib/lv2
+cp -r zynayumi.lv2 /usr/local/lib/lv2
+mkdir /usr/local/lib/dssi
+cp zynayumi-dssi.so /usr/local/lib/dssi
+mkdir /usr/local/lib/vst
+cp zynayumi-vst.so /usr/local/lib/vst
 cd ..
