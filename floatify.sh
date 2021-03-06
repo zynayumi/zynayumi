bdfdba53f5049a9c154e1861d452068e09b65d5f
Nil Geisweiller <ngeiswei@gmail.com>
2021-03-06 16:51:22 +0200

    Add support for negative integers in floatify.sh

--- floatify.sh
+++ floatify.sh
@@ -2,4 +2,4 @@
 
 # Replace integers by floats to workaround so possible incompatibility issues
 PRESETS_FILE=bin/zynayumi.lv2/presets.ttl
-sed 's/pset:value[[:space:]]\([[:digit:]]\+\)[[:space:]]/pset:value \1.0/' -i "${PRESETS_FILE}"
+sed 's/pset:value[[:space:]]\(-\?[[:digit:]]\+\)[[:space:]]/pset:value \1.0/' -i "${PRESETS_FILE}"
