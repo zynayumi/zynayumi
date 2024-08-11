3a06b247ed1881213885b7d16fc95c13745f7511
Nil Geisweiller <ngeiswei@gmail.com>
2024-08-11 16:07:23 +0300

    Add missing cstdint include for uint32_t

--- plugins/zynayumi/PluginZynayumi.hpp
+++ plugins/zynayumi/PluginZynayumi.hpp
@@ -5,18 +5,20 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef PLUGIN_ZYNAYUMI_HPP
 #define PLUGIN_ZYNAYUMI_HPP
+
+#include <cstdint>
 
 #include "DistrhoPlugin.hpp"
 #include "../../libzynayumi/src/zynayumi/zynayumi.hpp"
